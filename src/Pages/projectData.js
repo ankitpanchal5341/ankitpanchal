eda71000fa6e41888ebf04839a255538c54d2c5b
ankitpanchal534 <ankitpanchal534@gmail.com>
2023-04-06 21:01:18 +0530

    added project 4

--- src/Pages/projectData.js
+++ src/Pages/projectData.js
@@ -1,4 +1,4 @@
-import { project_image_1, project_image_2, project_image_3 } from "../assests";
+import { project_4, project_image_1, project_image_2, project_image_3 } from "../assests";
 
 export const projectData = [
     {
@@ -18,5 +18,12 @@ export const projectData = [
         description: 'Restaurant website templates to make your cafeteria, restaurant, pizzeria, and any other food-related website look cool on the internet. ',
         link: 'https://origo-assignment.vercel.app/',
         image: project_image_1
+    }
+    ,
+    {
+        name: 'Event Planner & Party Planner website (YASH PATEL LLC)',
+        description: 'Event Management & Birthday  Party , Office Party & other Party planner. This website is created for a UAE based startup . ',
+        link: 'https://yashpatelllc.com/',
+        image: project_4
     }
 ]
